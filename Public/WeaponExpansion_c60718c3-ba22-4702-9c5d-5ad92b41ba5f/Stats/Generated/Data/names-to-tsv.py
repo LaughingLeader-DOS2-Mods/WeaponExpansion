275b22c3680af998ba554386b08c8328c6d3ee24
LaughingLeader <laughingleader@gmail.com>
2019-10-28 08:46:45 -0500

    Removed a deprecated rod name, got the py script working

--- Public/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Stats/Generated/Data/names-to-tsv.py
+++ Public/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Stats/Generated/Data/names-to-tsv.py
@@ -2,6 +2,7 @@ import re
 
 pattern_group = r"new namegroup \"(?P<namegroup>.*?)\""
 pattern_name = r"add name(cool)? \"(?P<name>.*?)\",\"(?P<desc>.*?)\""
+pattern_blank = r"^\s*$"
 
 class NameEntry():
 	name = ""
@@ -16,22 +17,28 @@ class NameGroupEntry():
 
 	def __init__(self, name):
 		self.name = name
+		self.entries = []
 
-def get_namegroup(str):
-	matches = re.search(pattern_group, str)
+def get_namegroup(line):
+	matches = re.search(pattern_group, line)
 	if matches:
 		namegroup = matches.group("namegroup")
 		if namegroup != "":
 			return NameGroupEntry(namegroup)
 	return None
 
-def get_name(str):
-	matches = re.search(pattern_name, str)
+def get_name(line):
+	matches = re.search(pattern_name, line)
 	if matches:
-		name = matches.group("name")
-		desc = matches.group("desc")
+		name = matches.group(2)
+		desc = matches.group(3)
 		return NameEntry(name, desc)
 	return None
+
+def is_blank(line):
+	if not line.strip():
+		return True
+	return False
 
 import os
 
@@ -53,44 +60,58 @@ print("Total lines '{}'".format(maxLines))
 # for i in range(0, len(contents)):
 # 	print("Line '{}' = {}".format(i, contents[i]))
 
-def get_names(namegroup, line):
-	name_entry = get_name(line)
-	if name_entry != None:
-		namegroup.entries.append(name_entry)
-		return True
-	return False
-
 def get_line(lineNum, maxLines, contents):
 	if lineNum >= maxLines:
 		return False
 	return contents[lineNum]
 
+active_namegroup = None
+
 while lineNum < maxLines:
 	#print("Iterating '{}/{}'".format(lineNum,maxLines))
 	line = contents[lineNum]
-	namegroup = get_namegroup(line)
-	if namegroup != None:
-		namegroups.append(namegroup)
-		lineNum += 1
-		while get_names(namegroup, line) is True:
-			line = get_line(lineNum, maxLines, contents)
-			if line is False:
-				break
+	if active_namegroup is None:
+		namegroup = get_namegroup(line)
+		if namegroup != None:
+			active_namegroup = namegroup
+			namegroups.append(namegroup)
+			#print("Set active namegroup to {}".format(active_namegroup.name))
+	else:
+		if is_blank(line):
+			active_namegroup = None
+		else:
+			name_entry = get_name(line)
+			if name_entry != None:
+				active_namegroup.entries.append(name_entry)
+				#print("Appended entry '{}'".format(name_entry.name))
 	lineNum += 1
 
 print("{} total namegroups found.".format(len(namegroups)))
 
 output = ""
 
+# sort
+namegroups.sort(key=lambda x: x.name, reverse=False)
+
 for g in namegroups:
-	print("Namegroup: {}".format(g.name))
+	#print("Namegroup: {} | Entries: {}".format(g.name, len(g.entries)))
 	locale_name = "{}_DisplayName".format(g.name)
 	locale_description = "{}_Description".format(g.name)
-	for entry in g.entries:
+	append_num = len(g.entries) > 1
+	#for entry in g.entries:
+	for i in range(0, len(g.entries)):
+		entry = g.entries[i]
+		entry_output_name = locale_name
+		entry_output_desc = locale_description
+		if append_num:
+			entry_output_name = "{}_{}".format(locale_name, (i+1))
+			entry_output_desc = "{}_{}".format(locale_description, (i+1))
 		if entry.name != "":
-			output += "{}\t{}\n".format(locale_name, entry.name)
+			output += "{}\t{}\n".format(entry_output_name, entry.name)
 		if entry.description != "":
-			output += "{}\t{}\n".format(locale_description, entry.name)
+			output += "{}\t{}\n".format(entry_output_desc, entry.name)
 	#print("  Entries: {}".format(g.entries))
 
 output_path = os.path.join(script_dir, "ItemProgressionNames-test.tsv")
+file = open(output_path, 'w')
+file.write(output)
