7a60cef10fec6e346b5e519d8c99bee0ad746f3b
LaughingLeader <laughingleader@gmail.com>
2020-03-28 19:25:54 -0500

    Path slash fix

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/BootstrapClient.lua
@@ -14,5 +14,5 @@ Ext.RegisterListener("SkillGetDescriptionParam", LLWEAPONEX_GetDescriptionParam)
 Ext.Require("WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f", "LLWEAPONEX_StatOverrides.lua");
 Ext.Require("WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f", "LLWEAPONEX_SkillDamage.lua");
 
-Ext.AddPathOverride("Public\\Game\\GUI\\tooltip.swf", "Public\\WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f\\GUI\\LLWEAPONEX_ToolTip.swf")
+Ext.AddPathOverride("Public/Game/GUI/tooltip.swf", "Public/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/GUI/LLWEAPONEX_ToolTip.swf")
 Ext.Print("[WeaponExpansion] Enabled tooltip.swf override.")
--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared.lua
@@ -4,8 +4,8 @@ WeaponExpansion = {
     Debug = {}
 }
 
---Ext.AddPathOverride("Public\\Game\\GUI\\tooltip.swf", "Public\\WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f\\GUI\\LLWEAPONEX_ToolTip.swf")
+--Ext.AddPathOverride("Public/Game/GUI/tooltip.swf", "Public/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/GUI/LLWEAPONEX_ToolTip.swf")
 --Ext.Print("[WeaponExpansion] Enabled tooltip.swf override.")
---Ext.AddPathOverride("Public\\Game\\GUI\\tooltip.swf", "Public\\WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f\\GUI\\serverlist.swf")
---Ext.AddPathOverride("Public\\Game\\GUI\\tooltipHelper_kb.swf", "Public\\WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f\\GUI\\serverlist.swf")
---Ext.AddPathOverride("Public\\Game\\GUI\\tooltipHelper.swf", "Public\\WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f\\GUI\\serverlist.swf")
+--Ext.AddPathOverride("Public/Game/GUI/tooltip.swf", "Public/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/GUI/serverlist.swf")
+--Ext.AddPathOverride("Public/Game/GUI/tooltipHelper_kb.swf", "Public/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/GUI/serverlist.swf")
+--Ext.AddPathOverride("Public/Game/GUI/tooltipHelper.swf", "Public/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/GUI/serverlist.swf")
