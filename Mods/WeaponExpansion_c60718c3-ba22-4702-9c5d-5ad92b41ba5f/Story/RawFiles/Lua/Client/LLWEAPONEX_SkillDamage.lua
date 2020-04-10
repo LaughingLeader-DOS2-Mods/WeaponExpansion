529e1d509b2d0f431bf5e46adfe5164ee689eedf
LaughingLeader <laughingleader@gmail.com>
2020-04-09 22:23:44 -0500

    Update LLWEAPONEX_SkillDamage.lua

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Client/LLWEAPONEX_SkillDamage.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Client/LLWEAPONEX_SkillDamage.lua
@@ -1,4 +1,4 @@
-local function GetScaledPistolDamage(skill, attackerStats, isFromItem, stealthed, attackerPosition, targetPosition, level, noRandomization)
+local function GetScaledPistolDamage(skill, attacker, isFromItem, stealthed, attackerPos, targetPos, level, noRandomization)
 	local damageList = Ext.NewDamageList()
 	damageList:Add("Physical", 10)
 	return damageList
@@ -29,29 +29,26 @@ local skillprototype_debug = {
 	RootSkillPrototype = true,
 }
 
-local function LLWEAPONEX_GetSkillDamage(skill, attackerStats, isFromItem, stealthed, attackerPosition, targetPosition, level, noRandomization)
-	Ext.Print("Getting skill damage for: " .. tostring(skill) .. " isFromItem("..tostring(isFromItem)..") stealthed("..tostring(stealthed)..") attackerPosition("..LeaderLib.Common.Dump(attackerPosition)..") targetPosition("..tostring(targetPosition)..") level("..tostring(level)..") noRandomization("..tostring(noRandomization)..")")
+local function LLWEAPONEX_GetSkillDamage(skill, attacker, isFromItem, stealthed, attackerPos, targetPos, level, noRandomization)
 	local damageList = nil
-
+	local deathType = 1
 	for name,b in pairs(skillprototype_debug) do
 		Ext.Print("skill[\""..name.."\"] = ("..tostring(skill[name])..")")
 	end
 
-	local skill_func = skill_damage_functions[skill]
-	if skill["Icon"] == "unknown" then
-		skill_func = GetScaledPistolDamage
-	end
+	local skill_func = skill_damage_functions[skill.Name]
 	if skill_func ~= nil then
-		Ext.Print("attackerStats("..LeaderLib.Common.Dump(attackerStats)..")")
-		damageList = skill_func(skill, attackerStats, isFromItem, stealthed, attackerPosition, targetPosition, level, noRandomization)
+		local success = false
+		--Ext.Print("attackerStats("..LeaderLib.Common.Dump(attackerStats)..")")
+		success,damageList,deathType = pcall(skill_func, skill, attacker, isFromItem, stealthed, attackerPos, targetPos, level, noRandomization)
 	end
 	if damageList ~= nil then
 		Ext.Print("damageList("..tostring(LeaderLib.Common.Dump(damageList:ToTable()))..")")
 	end
-	return damageList, 1
+	return damageList, deathType
 end
 
---Ext.RegisterListener("GetSkillDamage", LLWEAPONEX_GetSkillDamage)
+Ext.RegisterListener("GetSkillDamage", LLWEAPONEX_GetSkillDamage)
 
 local skill_params = {
 	Target_LLWEAPONEX_Pistol_A_Shoot = {
