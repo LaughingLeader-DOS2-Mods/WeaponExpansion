cd9cc6763e110581de37f4e76fe9380212394858
LaughingLeader <laughingleader@gmail.com>
2020-04-22 18:59:21 -0500

    Added tag checks to the mastery param text, support showing a locked text

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Skills/MasteryBonuses.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Skills/MasteryBonuses.lua
@@ -1,55 +1,58 @@
+SKILL_STATE = LeaderLib.SKILL_STATE
+
 local throwingKnifeBonuses = {
 	"Projectile_LLWEAPONEX_DaggerMastery_ThrowingKnife_Poison",
 	"Projectile_LLWEAPONEX_DaggerMastery_ThrowingKnife_Explosive",
 }
 
-local function ThrowingKnife(char, state, funcParams)
+local function ThrowingKnifeBonus(char, state, funcParams)
 	--Ext.Print("[MasteryBonuses:ThrowingKnife] char(",char,") state(",state,") funcParams("..Ext.JsonStringify(funcParams)..")")
-	local hasMastery = true--IsTagged(char, "LLWEAPONEX_Dagger_Mastery1") == 1
-	local procSet = ObjectGetFlag(char, "LLWEAPONEX_ThrowingKnife_ActivateBonus") == 1
-
-	if state == WeaponExpansion.Skills.SKILL_STATE.USED then
-		if hasMastery and not procSet and Ext.Random(1,100) <= Ext.ExtraData["LLWEAPONEX_ThrowingKnife_MasteryBonusChance"] then
-			-- Position
-			if #funcParams == 3 then
-				local x = funcParams[1]
-				local y = funcParams[2]
-				local z = funcParams[3]
-				SetVarFloat3(char, "LLWEAPONEX_ThrowingKnife_ExplodePosition", x,y,z)
-			elseif funcParams[1] ~= nil then
-				local x,y,z = GetPosition(funcParams[1])
-				SetVarFloat3(char, "LLWEAPONEX_ThrowingKnife_ExplodePosition", x,y,z)
+	local hasMastery = IsTagged(char, "LLWEAPONEX_Dagger_Mastery1") == 1
+	if hasMastery then
+		local procSet = ObjectGetFlag(char, "LLWEAPONEX_ThrowingKnife_ActivateBonus") == 1
+		if state == SKILL_STATE.USED then
+			if hasMastery and not procSet and Ext.Random(1,100) <= Ext.ExtraData["LLWEAPONEX_ThrowingKnife_MasteryBonusChance"] then
+				-- Position
+				if #funcParams == 3 then
+					local x = funcParams[1]
+					local y = funcParams[2]
+					local z = funcParams[3]
+					SetVarFloat3(char, "LLWEAPONEX_ThrowingKnife_ExplodePosition", x,y,z)
+				elseif funcParams[1] ~= nil then
+					local x,y,z = GetPosition(funcParams[1])
+					SetVarFloat3(char, "LLWEAPONEX_ThrowingKnife_ExplodePosition", x,y,z)
+				end
+				ObjectSetFlag(char, "LLWEAPONEX_ThrowingKnife_ActivateBonus", 0)
 			end
-			ObjectSetFlag(char, "LLWEAPONEX_ThrowingKnife_ActivateBonus", 0)
-		end
-	elseif state == WeaponExpansion.Skills.SKILL_STATE.CAST then
-		if procSet then
-			Mods.LeaderLib.StartTimer("LLWEAPONEX_Daggers_ThrowingKnife_ProcBonus", 250, char)
-		end
-	elseif state == WeaponExpansion.Skills.SKILL_STATE.HIT then
-		if procSet then
-			local target = funcParams[1]
-			if target ~= nil then
-				Mods.LeaderLib.CancelTimer("LLWEAPONEX_Daggers_ThrowingKnife_ProcBonus", char)
-				local explodeSkill = throwingKnifeBonuses[Ext.Random(1,2)]
-				local level = CharacterGetLevel(char)
-				NRD_ProjectilePrepareLaunch()
-				NRD_ProjectileSetString("SkillId", explodeSkill)
-				NRD_ProjectileSetInt("CasterLevel", level)
-				NRD_ProjectileSetGuidString("Caster", char)
-				NRD_ProjectileSetGuidString("Source", char)
-				NRD_ProjectileSetGuidString("SourcePosition", target)
-				NRD_ProjectileSetGuidString("HitObject", target)
-				NRD_ProjectileSetGuidString("HitObjectPosition", target)
-				NRD_ProjectileSetGuidString("TargetPosition", target)
-				NRD_ProjectileLaunch();
+		elseif state == SKILL_STATE.CAST then
+			if procSet then
+				Mods.LeaderLib.StartTimer("LLWEAPONEX_Daggers_ThrowingKnife_ProcBonus", 250, char)
 			end
-			ObjectClearFlag(char, "LLWEAPONEX_ThrowingKnife_ActivateBonus", 0)
+		elseif state == SKILL_STATE.HIT then
+			if procSet then
+				local target = funcParams[1]
+				if target ~= nil then
+					Mods.LeaderLib.CancelTimer("LLWEAPONEX_Daggers_ThrowingKnife_ProcBonus", char)
+					local explodeSkill = throwingKnifeBonuses[Ext.Random(1,2)]
+					local level = CharacterGetLevel(char)
+					NRD_ProjectilePrepareLaunch()
+					NRD_ProjectileSetString("SkillId", explodeSkill)
+					NRD_ProjectileSetInt("CasterLevel", level)
+					NRD_ProjectileSetGuidString("Caster", char)
+					NRD_ProjectileSetGuidString("Source", char)
+					NRD_ProjectileSetGuidString("SourcePosition", target)
+					NRD_ProjectileSetGuidString("HitObject", target)
+					NRD_ProjectileSetGuidString("HitObjectPosition", target)
+					NRD_ProjectileSetGuidString("TargetPosition", target)
+					NRD_ProjectileLaunch();
+				end
+				ObjectClearFlag(char, "LLWEAPONEX_ThrowingKnife_ActivateBonus", 0)
+			end
 		end
 	end
 end
 
-LeaderLib.RegisterSkillListener("Projectile_ThrowingKnife", ThrowingKnife)
+LeaderLib.RegisterSkillListener("Projectile_ThrowingKnife", ThrowingKnifeBonus)
 
 local function ThrowingKnifeDelayedProc(funcParams)
 	local char = funcParams[1]
