b272aad9e015317782acd8906aa3a1cc8914fb10
LaughingLeader <laughingleader@gmail.com>
2020-04-26 23:04:38 -0500

    Setting up the great mastery data refactor

---
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Data/MasteryData.lua
@@ -0,0 +1,23 @@
+local TranslatedString = LeaderLib.Classes["TranslatedString"]
+
+---@class MasteryData
+local MasteryData = {
+	ID = "",
+	---@type TranslatedString
+	Name = {},
+}
+MasteryData.__index = MasteryData
+
+---@param id string
+---@param name string
+---@return MasteryData
+function MasteryData:Create(id,name)
+    local this =
+    {
+		ID = id,
+		Name = name
+	}
+	setmetatable(this, self)
+	WeaponExpansion.Masteries[id] = this
+    return this
+end
