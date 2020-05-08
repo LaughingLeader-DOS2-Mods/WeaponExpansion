d2061a5b6fb4db70662ae87ef7c891ebe4cd90e9
LaughingLeader <laughingleader@gmail.com>
2020-05-07 20:03:30 -0500

    Testing displaying a status tooltip

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/UI/MasteryMenuCommands.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/UI/MasteryMenuCommands.lua
@@ -1,3 +1,6 @@
+---@type MessageData
+local MessageData = LeaderLib.Classes["MessageData"]
+
 function SendClientID(uuid, id)
 	Ext.PostMessageToClient(uuid, "LLWEAPONEX_SetClientID", tostring(id))
 end
@@ -74,3 +77,46 @@ local function RequestOpenMasteryMenu(call,id,callbackID)
 end
 
 Ext.RegisterNetListener("LLWEAPONEX_RequestOpenMasteryMenu", RequestOpenMasteryMenu)
+
+local statusDummy = "S_LeaderLib_Dummy_TargetHelper_A_36069245-0e2d-44b1-9044-6797bd29bb15"
+local function RequestStatusTooltip(call,datastr)
+	LeaderLib.PrintDebug("[WeaponExpansion:MasteryMenuCommands.lua:LLWEAPONEX_MasteryMenu_RequestStatusTooltip] Data:")
+	LeaderLib.PrintDebug(datastr)
+	local data = MessageData:CreateFromString(datastr)
+	if data ~= nil then
+		local character = data.ID
+		local status = data.Params.Status
+		if status ~= nil then
+			local handle = NRD_StatusGetHandle(statusDummy, status)
+			if handle == nil then
+				ApplyStatus(statusDummy, status, -1.0, 1, statusDummy)
+				handle = NRD_StatusGetHandle(statusDummy, status)
+			end
+			---@type EsvStatus
+			local statusObj = Ext.GetStatus(statusDummy, handle)
+			data.Params.StatusHandle = statusObj.StatusHandle
+			data.Params.NetID = Ext.GetCharacter(statusDummy).NetID
+			local statuses = Ext.GetCharacter(statusDummy):GetStatuses()
+			--CharacterSetForceSynch(statusDummy, 1)
+			--CharacterSetForceUpdate(statusDummy, 1)
+			--CharacterSetForceSynch(statusDummy, 0)
+			--CharacterSetForceUpdate(statusDummy, 0)
+			local host = CharacterGetHostCharacter()
+			SetOnStage(statusDummy, 1)
+			CharacterSetDetached(statusDummy, 0)
+			--CharacterMakePlayer(statusDummy, host)
+			TeleportTo(statusDummy, host, "", 0, 1, 1)
+			--CharacterMakeNPC(statusDummy)
+			data.Params.Host = host
+
+			local resultStr = data:ToString()
+			LeaderLib.PrintDebug("[WeaponExpansion:MasteryMenuCommands.lua:LLWEAPONEX_MasteryMenu_RequestStatusTooltip] Sending data back to client:")
+			LeaderLib.PrintDebug(resultStr)
+			Ext.PostMessageToClient(character, "LLWEAPONEX_MasteryMenu_StatusHandleRetrieved", resultStr)
+		end
+	else
+		Ext.PrintError("MessageData:CreateFromString failed.")
+	end
+end
+
+Ext.RegisterNetListener("LLWEAPONEX_MasteryMenu_RequestStatusTooltip", RequestStatusTooltip)
