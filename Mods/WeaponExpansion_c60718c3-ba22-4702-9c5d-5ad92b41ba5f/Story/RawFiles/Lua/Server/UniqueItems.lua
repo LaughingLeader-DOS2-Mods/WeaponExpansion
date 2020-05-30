8c47306688fe746702c48d3e69d2682948a8a1f2
LaughingLeader <laughingleader@gmail.com>
2020-05-30 18:30:03 -0500

    Setting up a more robut unique swapping script

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/UniqueItems.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/UniqueItems.lua
@@ -1,6 +1,6 @@
 local VENDING_MACHINE = "S_LLWEAPONEX_VendingMachine_A_680d2702-721c-412d-b083-4f5e816b945a"
 
-local AllUniques = {
+AllUniques = {
 	"S_EQ_UNIQUE_LLWEAPONEX_ArmCannon_A_a1ce4c1c-a535-4184-a1df-268eb4035fe8",
 	"S_WPN_UNIQUE_LLWEAPONEX_Katana_Dagger_Sword_2H_Muramasa_52c0b4a4-3906-4229-93a9-b83aea9e657c",
 	"S_WPN_UNIQUE_LLWEAPONEX_Rapier_Dagger_Runeblade_Water_1H_5d8ec362-618e-48e9-87c2-dbc18ea4e779",
@@ -30,3 +30,111 @@ local AllUniques = {
 	"S_EQ_UNIQUE_LLWEAPONEX_DemonGauntlet_Arms_A_0ac0d813-f58c-4399-99a8-1626a419bc53",
 	"S_EQ_UNIQUE_LLWEAPONEX_HandCrossbow_A_Ring_70c59769-2838-4137-9421-4e251fecdc89",
 }
+
+local LinkedUniques = {
+	["MagicMissileWand"] = {
+		{"S_WPN_UNIQUE_LLWEAPONEX_Wand_1H_MagicMissile_A_f8958c1e-1c9d-4fa9-b03f-b883c65f95c3", "S_WPN_UNIQUE_LLWEAPONEX_Rod_1H_MagicMissile_A_292b4b04-4ba1-4fa3-96df-19eab320c50f"}
+	},
+	["Bokken"] = {
+		{"S_WPN_UNIQUE_LLWEAPONEX_Bokken_Sword_2H_A_6d75d449-e021-4b4d-ad2d-c0873127c3b3",  "S_WPN_UNIQUE_LLWEAPONEX_Bokken_Sword_1H_A_a5e7e46f-b83a-47a7-8bd6-f16f16fe5f42"}
+	},
+	["Warchief"] = {
+		{"S_WPN_UNIQUE_LLWEAPONEX_Spear_Halberd_2H_Warchief_A_6c52f44e-1c27-4409-9bfe-f89ee5af4a0d",  "S_WPN_UNIQUE_LLWEAPONEX_Axe_Halberd_2H_Warchief_A_001_056c2c38-b7be-4e06-be41-99b79ffe83c2"}
+	}
+}
+
+local function AddLinkedUnique(id, item1, item2)
+	if item1 ~= nil and item2 ~= nil then
+		local tbl = LinkedUniques[id]
+		if tbl ~= nil then
+			local canAdd = true
+			local uuid1 = GetUUID(item1)
+			local uuid2 = GetUUID(item2)
+			for i,v in pairs(tbl) do
+				local a,b = table.unpack(v)
+				if GetUUID(a) == uuid1 and GetUUID(b) == uuid2 then
+					canAdd = false
+					break
+				end
+			end
+			if canAdd then
+				table.insert(tbl, {item1,item2})
+			end
+		else
+			LinkedUniques[id] = {
+				{item1,item2}
+			}
+		end
+		PersistentVars.LinkedUniques = LinkedUniques
+	end
+
+	print("AddLinkedUnique", Ext.JsonStringify(LinkedUniques))
+end
+
+LoadPersistentVars[#LoadPersistentVars+1] = function()
+	if PersistentVars.LinkedUniques ~= nil then
+		for id,entry in pairs(LoadPersistentVars.LinkedUniques) do
+			if #entry >= 2 then
+				for i,v in pairs(entry) do
+					local item1,item2 = table.unpack(v)
+					AddLinkedUnique(id, item1, item2)
+				end
+			end
+		end
+	else
+		PersistentVars.LinkedUniques = LinkedUniques
+	end
+end
+
+function SwapUnique(char, id)
+	local equipped = nil
+	local next = nil
+	local tbl = LinkedUniques[id]
+	if tbl ~= nil then
+		for i,v in pairs(tbl) do
+			local item1,item2 = table.unpack(v)
+			if GameHelpers.ItemIsEquipped(char, item1) then
+				equipped = item1
+				next = item2
+				break
+			elseif GameHelpers.ItemIsEquipped(char, item2) then
+				equipped = item2
+				next = item1
+				break
+			end
+		end
+	end
+	if equipped ~= nil and next ~= nil then
+		local isTwoHanded = Ext.StatGetAttribute(NRD_ItemGetStatsId(next), "IsTwoHanded") == "Yes"
+		local slot = GameHelpers.GetEquippedSlot(char,equipped)
+		CharacterUnequipItem(char, equipped)
+		--ItemToInventory(equippedItem, targetItem, 1, 0, 0)
+		if not isTwoHanded then
+			NRD_CharacterEquipItem(char, next, slot, 0, 0, 1, 1)
+		else
+			NRD_CharacterEquipItem(char, next, "Weapon", 0, 0, 1, 1)
+		end
+		Osi.LeaderLib_Timers_StartObjectObjectTimer(equipped, next, 50, "Timers_LLWEAPONEX_MoveMagicMissileWeapon", "LeaderLib_Commands_ItemToInventory")
+	end
+end
+
+local SkillSlots = {}
+
+function SaveSkillSlot(char, skill)
+	if SkillSlots[char] == nil then
+		SkillSlots[char] = {}
+	end
+	SkillSlots[char][skill] = NRD_SkillBarFindSkill(char, skill)
+end
+
+function RestoreSkillSlot(char, previousSkill, replacementSkill)
+	if SkillSlots[char] ~= nil then
+		local nextSlot = SkillSlots[char][previousSkill]
+		if nextSlot ~= nil then
+			local currentSlot = NRD_SkillBarFindSkill(char, replacementSkill)
+			NRD_SkillBarClear(char, currentSlot)
+			NRD_SkillBarSetSkill(char, nextSlot, replacementSkill)
+			SkillSlots[char][previousSkill] = nil
+		end
+	end
+end
