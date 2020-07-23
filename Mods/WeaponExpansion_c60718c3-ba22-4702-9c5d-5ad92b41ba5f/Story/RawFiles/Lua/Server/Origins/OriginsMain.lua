b5965701d925be3fd54426b0f7e2d2d5e4a82af3
LaughingLeader <laughingleader@gmail.com>
2020-07-23 01:41:19 -0500

    Set some origin setup commands

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Origins/OriginsMain.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Origins/OriginsMain.lua
@@ -1,4 +1,4 @@
-OriginCharacters = {
+Mercs = {
 	-- S_Player_LLWEAPONEX_Harken_e446752a-13cc-4a88-a32e-5df244c90d8b
 	Harken = "e446752a-13cc-4a88-a32e-5df244c90d8b",
 	-- S_Player_LLWEAPONEX_Korvash_3f20ae14-5339-4913-98f1-24476861ebd6
@@ -7,29 +7,31 @@ OriginCharacters = {
 
 function Origins_InitCharacters(region, isEditorMode)
 	--IsCharacterCreationLevel(region) == 0
-	if CharacterIsPlayer(OriginCharacters.Harken) == 0 or isEditorMode == 1 then
-		CharacterApplyPreset(OriginCharacters.Harken, "Knight_Act2")
-		GameHelpers.UnequipItemInSlot(OriginCharacters.Harken, "Weapon", true)
-		GameHelpers.UnequipItemInSlot(OriginCharacters.Harken, "Helmet", true)
-		Uniques.AnvilMace:Transfer(OriginCharacters.Harken, true)
+	if CharacterIsPlayer(Mercs.Harken) == 0 or isEditorMode == 1 then
+		CharacterApplyPreset(Mercs.Harken, "Knight_Act2")
+		GameHelpers.UnequipItemInSlot(Mercs.Harken, "Weapon", true)
+		GameHelpers.UnequipItemInSlot(Mercs.Harken, "Helmet", true)
+		Uniques.AnvilMace:Transfer(Mercs.Harken, true)
 	end
-	Uniques.HarkenPowerGloves:Transfer(OriginCharacters.Harken, true)
+	Uniques.HarkenPowerGloves:Transfer(Mercs.Harken, true)
 	
-	if CharacterIsPlayer(OriginCharacters.Korvash) == 0 or isEditorMode == 1 then
-		CharacterApplyPreset(OriginCharacters.Korvash, "Inquisitor_Act2")
-		GameHelpers.UnequipItemInSlot(OriginCharacters.Korvash, "Weapon", true)
-		GameHelpers.UnequipItemInSlot(OriginCharacters.Korvash, "Helmet", true)
-		Uniques.DeathEdge:Transfer(OriginCharacters.Korvash, true)
+	if CharacterIsPlayer(Mercs.Korvash) == 0 or isEditorMode == 1 then
+		CharacterApplyPreset(Mercs.Korvash, "Inquisitor_Act2")
+		GameHelpers.UnequipItemInSlot(Mercs.Korvash, "Weapon", true)
+		GameHelpers.UnequipItemInSlot(Mercs.Korvash, "Helmet", true)
+		Uniques.DeathEdge:Transfer(Mercs.Korvash, true)
 	end
-	Uniques.DemonHand:Transfer(OriginCharacters.Korvash, true)
+	Uniques.DemonGauntlet:Transfer(Mercs.Korvash, true)
 
 	if Ext.IsDeveloperMode() or isEditorMode == 1 then
 		local host = GetUUID(CharacterGetHostCharacter())
-		if host ~= OriginCharacters.Harken then
-			Osi.PROC_GLO_PartyMembers_Add(OriginCharacter.Harken, host)
+		if host ~= Mercs.Harken then
+			Osi.PROC_GLO_PartyMembers_Add(Mercs.Harken, host)
+			TeleportTo(Mercs.Harken, host, "", 1, 0, 1)
 		end
-		if host ~= OriginCharacters.Korvash then
-			Osi.PROC_GLO_PartyMembers_Add(OriginCharacter.Korvash, host)
+		if host ~= Mercs.Korvash then
+			Osi.PROC_GLO_PartyMembers_Add(Mercs.Korvash, host)
+			TeleportTo(Mercs.Korvash, host, "", 1, 0, 1)
 		end
 	end
 end
