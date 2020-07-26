1b3f21ea0c742bd0da2995d931034cfebce0f9d6
LaughingLeader <laughingleader@gmail.com>
2020-07-25 23:56:24 -0500

    Updated skill refreshing (tag requirements changing)

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Client/SkillParams.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Client/SkillParams.lua
@@ -206,29 +206,33 @@ local function GetDamageParamResult(param_func, skill, character, isFromItem)
 	local status,damageRange = xpcall(param_func, debug.traceback, skill, character, isFromItem, false, defaultPos, defaultPos, -1, 0, true)
 	if status then
 		if damageRange ~= nil then
-			local damageTexts = {}
-			local totalDamageTypes = 0
-			for damageType,damage in pairs(damageRange) do
-				local min = damage[1]
-				local max = damage[2]
-				if min == nil then min = 0 end
-				if max == nil then max = 0 end
-	
-				if min > 0 and max > 0 then
-					if max == min then
-						table.insert(damageTexts, GameHelpers.GetDamageText(damageType, string.format("%i", max)))
+			if type(damageRange) == "table" then
+				local damageTexts = {}
+				local totalDamageTypes = 0
+				for damageType,damage in pairs(damageRange) do
+					local min = damage[1]
+					local max = damage[2]
+					if min == nil then min = 0 end
+					if max == nil then max = 0 end
+		
+					if min > 0 and max > 0 then
+						if max == min then
+							table.insert(damageTexts, GameHelpers.GetDamageText(damageType, string.format("%i", max)))
+						else
+							table.insert(damageTexts, GameHelpers.GetDamageText(damageType, string.format("%i-%i", min, max)))
+						end
+					end
+					totalDamageTypes = totalDamageTypes + 1
+				end
+				if totalDamageTypes > 0 then
+					if totalDamageTypes > 1 then
+						return LeaderLib.Common.StringJoin(", ", damageTexts)
 					else
-						table.insert(damageTexts, GameHelpers.GetDamageText(damageType, string.format("%i-%i", min, max)))
+						return damageTexts[1]
 					end
 				end
-				totalDamageTypes = totalDamageTypes + 1
-			end
-			if totalDamageTypes > 0 then
-				if totalDamageTypes > 1 then
-					return LeaderLib.Common.StringJoin(", ", damageTexts)
-				else
-					return damageTexts[1]
-				end
+			else
+				return damageRange
 			end
 		end
 	else
