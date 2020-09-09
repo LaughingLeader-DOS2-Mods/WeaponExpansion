d96b694d60600c142da34d57bb690535dbc35d81
LaughingLeader <laughingleader@gmail.com>
2020-09-08 21:30:43 -0500

    Reworking WeaponEx table merges into a scripted solution, to add to categories

---
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Items/TreasureTableMerging.lua
@@ -0,0 +1,87 @@
+---@return StatTreasureCategory
+local function CreateBasicCategory(name, isTable, rarities)
+	local tbl = {Frequency = 1}
+
+	if isTable == true then
+		tbl.TreasureTable = "T_"..name
+	else
+		tbl.TreasureCategory = name
+	end
+
+	if rarities ~= nil then
+		for key,v in pairs(rarities) do
+			tbl[key] = v
+		end
+	end
+	return tbl
+end
+
+---@type table<string, table<string, StatTreasureCategory[]>>
+local TreasureTableOverrides = {
+	ST_WandNormal = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_RodsNormal", false, {Common=1})
+		}
+	},
+	ST_TwoHandedNormal = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_TwoHandedNormal", false, {Common=1})
+		}
+	},
+	ST_OneHandedNormal = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_OneHandedNormal", true, {Common=1})
+		}
+	},
+	ST_RangedNormal = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_RangedNormal", true, {Common=1})
+		}
+	},
+	ST_RingAmuletBelt = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_RingAmuletBelt", true, {Common=1})
+		}
+	},
+	ST_Trader_OneHandedNormal = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_Trader_OneHandedNormal", true, {Common=1})
+		}
+	},
+	ST_Trader_TwoHandedNormal = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_Trader_TwoHandedNormal", true, {Common=1})
+		}
+	},
+	ST_Trader_RangedNormal = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_Trader_RangedNormal", true, {Common=1})
+		}
+	},
+	ST_Trader_RingAmuletBeltNormal = {
+		Categories = {
+			CreateBasicCategory("ST_LLWEAPONEX_Trader_RingAmuletBeltNormal", true, {Common=1})
+		}
+	},
+}
+
+Ext.RegisterListener("SessionLoaded", function()
+	for statName,data in pairs(TreasureTableOverrides) do
+		local treasureTable = Ext.GetTreasureTable(statName)
+		if treasureTable ~= nil then
+			local sub = treasureTable.SubTables[1]
+			if sub ~= nil and data.Categories ~= nil then
+				for _,entry in pairs(data.Categories) do
+					table.insert(sub.Categories, entry)
+					if sub.TotalFrequency ~= nil then
+						sub.TotalFrequency = sub.TotalFrequency + entry.Frequency
+					end
+					if Ext.IsDeveloperMode() then
+						Ext.Print("Added category",entry.TreasureTable or entry.TreasureCategory,"to",statName)
+					end
+				end
+				Ext.UpdateTreasureTable(treasureTable)
+			end
+		end
+	end
+end)
