147c6e061006cd2e814311430f1a58f3d576f1bf
LaughingLeader <laughingleader@gmail.com>
2020-10-06 00:50:09 -0500

    Text / menu update

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Settings.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Settings.lua
@@ -3,9 +3,12 @@ return function()
 	local settings = LeaderLib.CreateModSettings("c60718c3-ba22-4702-9c5d-5ad92b41ba5f")
 	---@type TranslatedString
 	local ts = LeaderLib.Classes.TranslatedString
+
+	local MenuSectionGeneral = ts:Create("h6145e729g4cb0g4018gafe9gd89175b8943d", "General")
+	local MenuSectionUniques = ts:Create("h903efedcge7b3g485ag87bfgc121d0ecad7f", "<font color='#C7A758'>Unique Items</font>")
 	
 	settings.Global:AddLocalizedFlags({
-		"LLWEAPONEX_NoArmCannonRestrictions",
+		"LLWEAPONEX_ArmCannonRestrictionsDisabled",
 		"LLWEAPONEX_RemoteChargeDetonationCountDisabled",
 		"LLWEAPONEX_DemolitionBackpackAutoRechargeEnabled",
 		"LLWEAPONEX_ThrowObjectLimitDisabled",
@@ -19,9 +22,21 @@ return function()
 		
 	end
 
-	-- settings.GetMenuOrder = function()
-		
-	-- end
+	settings.GetMenuOrder = function()
+		return {{
+			DisplayName = MenuSectionGeneral.Value,
+			Entries = {
+				"LLWEAPONEX_AutoUnlockRecipesDisabled",
+				"LLWEAPONEX_ThrowObjectLimitDisabled",
+			}},
+			{DisplayName = MenuSectionUniques.Value, 
+			Entries = {		
+				"LLWEAPONEX_RemoteChargeDetonationCountDisabled",
+				"LLWEAPONEX_DemolitionBackpackAutoRechargeEnabled",
+				"LLWEAPONEX_ArmCannonRestrictionsDisabled",
+			}},
+		}
+	end
 
 	return settings
 end
