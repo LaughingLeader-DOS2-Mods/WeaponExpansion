46404777e505c1e6cc1b380c7469fb50fd5610bc
LaughingLeader <laughingleader@gmail.com>
2020-10-08 16:28:58 -0500

    Make shield bash nudge the target, moved armor restoration to script for DU support

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Mastery/Bonuses/DualShields.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Mastery/Bonuses/DualShields.lua
@@ -12,3 +12,28 @@ MasteryBonusManager.RegisterSkillTypeListener("rush", {"DUALSHIELDS_RUSHPROTECTI
 		Osi.LeaderLib_Timers_StartObjectTimer(char, 1500, "Timers_LLWEAPONEX_ResetDualShieldsRushProtection", "LLWEAPONEX_ResetDualShieldsRushProtection")
 	end
 end)
+
+---@param skill string
+---@param char string
+---@param state SkillState
+---@param data HitData
+LeaderLib.RegisterSkillListener("Target_LLWEAPONEX_ShieldBash", function(skill, char, state, data)
+	if state == SKILL_STATE.HIT and data.Success then
+		Osi.LeaderLib_Force_Apply(char, data.Target, 1)
+	end
+end)
+
+---@param skill string
+---@param char string
+---@param state SkillState
+---@param data SkillEventData
+LeaderLib.RegisterSkillListener("Shout_LLWEAPONEX_DualShields_HunkerDown", function(skill, char, state, data)
+	if state == SKILL_STATE.CAST then
+		if Ext.IsModLoaded(MODID.DivinityUnleashed) or Ext.IsModLoaded(MODID.ArmorMitigation) then
+
+		else
+			ApplyStatus(char, "SHIELDED_PHYSICAL", 0.0, 0, char)
+			ApplyStatus(char, "SHIELDED_MAGIC", 0.0, 0, char)
+		end
+	end
+end)
