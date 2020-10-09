4a3c496d1f01828fea83f6d8df294d7305dbb7f0
LaughingLeader <laughingleader@gmail.com>
2020-10-08 20:37:46 -0500

    Add throwing mastery xp when throwing Balrin's axe

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Mastery/Bonuses/Throwing.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Mastery/Bonuses/Throwing.lua
@@ -1,1 +1,17 @@
+function GainThrowingMasteryXP(uuid, target)
+	if IsPlayer(uuid) then
+		MasterySystem.GrantWeaponSkillExperience(uuid, target, "LLWEAPONEX_ThrowingAbility")
+	end
+end
 
+---@param skill string
+---@param char string
+---@param state SkillState
+---@param data SkillEventData|HitData
+local function OnBalrinAxeThrow(skill, char, state, data)
+	if state == SKILL_STATE.HIT and data.Success then
+		GainThrowingMasteryXP(char, data.Target)
+	end
+end
+LeaderLib.RegisterSkillListener("Projectile_LLWEAPONEX_Throw_UniqueAxe_A", OnBalrinAxeThrow)
+LeaderLib.RegisterSkillListener("Projectile_LLWEAPONEX_Throw_UniqueAxe_A_Offhand", OnBalrinAxeThrow)
