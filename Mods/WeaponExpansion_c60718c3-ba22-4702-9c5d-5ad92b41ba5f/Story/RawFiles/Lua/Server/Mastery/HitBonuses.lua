7d18219c09cdc6b6daa3af53b829faa7ca4e0f9c
LaughingLeader <laughingleader@gmail.com>
2021-06-18 12:43:07 -0500

    Switching to RegisterOnWeaponTypeHit

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Mastery/HitBonuses.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Server/Mastery/HitBonuses.lua
@@ -6,17 +6,17 @@ local function OnWandHit(target,source,damage,handle)
 
 end
 
---HitHandler.OnHitCallbacks["LLWEAPONEX_Wand"] = OnWandHit
-
-RegisterHitListener("OnHit", "LLWEAPONEX_BattleBook", function(target, source, damage, hit, context, status, masteryBonuses, tag)
-	local chance = GameHelpers.GetExtraData("LLWEAPONEX_MasteryBonus_Hit_BattleBook_ConcussionChance", 25.0)
-	if chance > 0 then
-		--HasActiveStatus(target, "LLWEAPONEX_CONCUSSION") == 0 
-		if HasMasteryLevel(source.MyGuid, "LLWEAPONEX_BattleBook", 1) then
-			if Ext.Random(0,100) <= chance then
-				local turns = GameHelpers.GetExtraData("LLWEAPONEX_MasteryBonus_Hit_BattleBook_ConcussionTurns", 1)
-				ApplyStatus(target.MyGuid, "LLWEAPONEX_CONCUSSION", 6.0, turns * 6.0, source.MyGuid)
-				PlaySound(target.MyGuid, "LLWEAPONEX_FFT_Dictionary_Book_Hit")
+BasicAttackManager.RegisterOnWeaponTypeHit("LLWEAPONEX_BattleBook", function(IsFromSkill, source, target, damage, data, bonuses, skill)
+	if not IsFromSkill then
+		local chance = GameHelpers.GetExtraData("LLWEAPONEX_MasteryBonus_Hit_BattleBook_ConcussionChance", 25.0)
+		if chance > 0 then
+			--HasActiveStatus(target, "LLWEAPONEX_CONCUSSION") == 0 
+			if HasMasteryLevel(source.MyGuid, "LLWEAPONEX_BattleBook", 1) then
+				if Ext.Random(0,100) <= chance then
+					local turns = GameHelpers.GetExtraData("LLWEAPONEX_MasteryBonus_Hit_BattleBook_ConcussionTurns", 1)
+					ApplyStatus(target.MyGuid, "LLWEAPONEX_CONCUSSION", 6.0, turns * 6.0, source.MyGuid)
+					PlaySound(target.MyGuid, "LLWEAPONEX_FFT_Dictionary_Book_Hit")
+				end
 			end
 		end
 	end
