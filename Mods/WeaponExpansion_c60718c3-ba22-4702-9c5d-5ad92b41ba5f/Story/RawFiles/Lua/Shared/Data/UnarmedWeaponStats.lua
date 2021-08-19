501a49da059c913d2ff33b9b74173ede8a861307
LaughingLeader <laughingleader@gmail.com>
2021-08-18 20:15:54 -0500

    Added a helper for registered an unarmed armor to weapon stat association

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Data/UnarmedWeaponStats.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Data/UnarmedWeaponStats.lua
@@ -1,5 +1,16 @@
--- Unarmed weapons are armor items that modify the damage of NoWeapon on hit
-
+---Unarmed weapons are armor items that modify the damage of NoWeapon on hit
+---ArmorStat => WeaponStat
+---@type table<string,string>
 UnarmedWeaponStats = {
 	--ARM_UNIQUE_LLWEAPONEX_DragonBoneClaws_A = "WPN_UNIQUE_LLWEAPONEX_DragonBoneClaws_A"
 }
+
+if UnarmedHelpers == nil then
+	UnarmedHelpers = {}
+end
+
+---@param armorStat string The armor stat to associate with a weapon stat, such as ARM_UNIQUE_LLWEAPONEX_DragonBoneClaws_A.
+---@param weaponStat string The weapon stat to use for damage calculation, such as WPN_UNIQUE_LLWEAPONEX_DragonBoneClaws_A.
+function UnarmedHelpers.RegisterUnarmedStat(armorStat, weaponStat)
+	UnarmedWeaponStats[armorStat] = weaponStat
+end
