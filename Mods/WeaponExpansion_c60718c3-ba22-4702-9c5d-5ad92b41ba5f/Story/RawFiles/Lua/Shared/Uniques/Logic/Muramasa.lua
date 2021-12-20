ea219043af9fe8d2cd8d6652ccc437f26e02ecbd
LaughingLeader <laughingleader@gmail.com>
2021-12-20 17:51:14 -0600

    Move Muramasa logic to Lua

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/Muramasa.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/Muramasa.lua
@@ -1,30 +1,22 @@
+Uniques.Muramasa:RegisterEquippedListener(function(unique, character, item, equipped)
+	if equipped then
+		if character.Stats.CurrentVitality <= (character.Stats.MaxVitality/2) then
+			GameHelpers.Status.Apply(character, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX", -1.0, false, character)
+		end
+	else
+		RemoveStatus(character.MyGuid, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX")
+	end
+end)
+
 if not Vars.IsClient then
-	RegisterSkillListener({"Projectile_SkyShot", "Projectile_EnemySkyShot"}, function(skill, char, state, data)
-		if GameHelpers.CharacterOrEquipmentHasTag(char, "LLWEAPONEX_Omnibolt_Equipped") then
-			if state == SKILL_STATE.HIT and data.Success then
-				GameHelpers.Skill.Explode(data.Target, "Projectile_LLWEAPONEX_Greatbow_LightningStrike", char)
-			elseif state == SKILL_STATE.CAST then
-				if data.TotalTargetPositions > 0 and data.TotalTargetObjects == 0 then
-					local x,y,z = table.unpack(data.TargetPositions[1])
-					Timer.Start("Timers_LLWEAPONEX_ProcGreatbowLightningStrike", 750, {
-						Pos={x,y,z},
-						Source=char
-					})
-				end
+	RegisterProtectedOsirisListener("CharacterVitalityChanged", 2, "after", function(char, percentage)
+		if GameHelpers.CharacterOrEquipmentHasTag(char, "LLWEAPONEX_UniqueMuramasaKatana") then
+			local hasWeaponEffect = HasActiveStatus(char, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX") == 1
+			if percentage <= 50 and not hasWeaponEffect then
+				GameHelpers.Status.Apply(char, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX", -1.0, false, char)
+			elseif percentage > 50 and hasWeaponEffect then
+				RemoveStatus(char, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX")
 			end
 		end
-	end)
-
-	Timer.RegisterListener("Timers_LLWEAPONEX_ProcGreatbowLightningStrike", function(timerName, char)
-		if char and ObjectGetFlag(char, "LLWEAPONEX_Omnibolt_SkyShotWorldBonus") == 1 then
-			local x,y,z = GetVarFloat3(char, "LLWEAPONEX_Omnibolt_SkyShotWorldPosition")
-			GameHelpers.Skill.Explode({x,y,z}, "Projectile_LLWEAPONEX_Greatbow_LightningStrike", char)
-		end
-	end)
-
-	AttackManager.OnWeaponTagHit.Register("LLWEAPONEX_Omnibolt_Equipped", function(tag, attacker, target, data, targetIsObject, skill)
-		if not skill and BonusRoll(GameHelpers.GetExtraData("LLWEAPONEX_Omnibolt_LightningChance", 201, true)) then
-			GameHelpers.Skill.Explode(target, "Projectile_LLWEAPONEX_Greatbow_LightningStrike", attacker, {EnemiesOnly = true})
-		end
-	end)
+	end, false)
 end
