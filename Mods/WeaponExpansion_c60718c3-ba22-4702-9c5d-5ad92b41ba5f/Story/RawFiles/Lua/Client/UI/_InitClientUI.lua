b2ecc87c27df1e3ca58931787b01c8426cbc4a6d
LaughingLeader <laughingleader@gmail.com>
2022-04-02 13:54:51 -0500

    Removed deprecated functions/listeners

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Client/UI/_InitClientUI.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Client/UI/_InitClientUI.lua
@@ -1,16 +1,6 @@
----@type MessageData
-local MessageData = LeaderLib.Classes["MessageData"]
-
 Ext.Require("Client/UI/ClientUIData.lua")
 Ext.Require("Client/UI/MasteryMenu/_Init.lua")
 local tooltipHandler = Ext.Require("Client/UI/TooltipHandler.lua")
-
-local function LLWEAPONEX_Client_ModuleSetup()
-	for filepath,overridepath in pairs(uiOverrides) do
-		Ext.AddPathOverride(filepath, overridepath)
-		Ext.Print("[LLWEAPONEX:Client:UI.lua] Enabled UI override ("..filepath..") => ("..overridepath..").")
-	end
-end
 
 --[[ 
 addSecondaryStat(statType:Number, labelText:String, valueText:String, tooltipStatId:Number, iconFrame:Number, boostValue:Number)
@@ -81,74 +71,11 @@ Ext.RegisterNetListener("LLWEAPONEX_OnCharacterCreationStarted", function(call, 
 		Ext.PostMessageToServer("LLWEAPONEX_CC_CheckKorvashColor", uuid)
 	end
 end)
--- Ext.RegisterNetListener("LLWEAPONEX_OnCharacterCreationFinished", function(call, uuid)
-	
--- end)
-
----@param ui UIObject
----@param call string
----@param isVisible boolean
-local function OnShowSkillBar(ui, call, isVisible)
-	if isVisible ~= nil and SharedData.RegionData.LevelType ~= LEVELTYPE.CHARACTER_CREATION then
-		if MasteryMenu.ToggleButtonInstance ~= nil then
-			MasteryMenu.RepositionToggleButton(MasteryMenu.ToggleButtonInstance, nil, nil, isVisible)
-		end
-	end
-end
-
-local ccUIMirrorHeaderText = Ext.GetTranslatedString("hf11e1d54g4cb6g4950g91e4g4d006ef46f15", "Magic Mirror")
-
----@param ui UIObject
----@param call string
-local function OnCCEvent(ui, call, param1, param2)
-	if call == "setText" and param2 == ccUIMirrorHeaderText then
-		MasteryMenu.SetToggleButtonVisibility(false, false) 
-	elseif call == "creationDone" or call == "updatePortraits" and param1 == false then
-		--MasteryMenu.SetToggleButtonVisibility(false, true) 
-	end
-end
 
 Ext.RegisterUITypeInvokeListener(Data.UIType.characterSheet, "updateArraySystem", OnCharacterSheetUpdating)
-Ext.RegisterUITypeInvokeListener(Data.UIType.hotBar, "showSkillBar", OnShowSkillBar)
 
-local function LLWEAPONEX_Client_SessionLoaded()
-	tooltipHandler.Init()
-end
-
-Ext.RegisterListener("SessionLoaded", LLWEAPONEX_Client_SessionLoaded)
---Ext.RegisterNetListener("LLWEAPONEX_LuaWasReset", LLWEAPONEX_Client_SessionLoaded)
-
-Ext.RegisterConsoleCommand("gender", function(cmd)
-	local ui = Ext.GetBuiltinUI("Public/Game/GUI/characterCreation.swf")
-	if ui ~= nil then
-		local main = ui:GetRoot()
-		main.setDetails(0, true)
-	end
-end)
-
-local function LLWEAPONEX_OnClientMessage(call,param)
-	if param == "HookUI" then
-		LLWEAPONEX_Client_SessionLoaded()
-	end
-end
-
-local function LLWEAPONEX_UpdateStatusMC(call,datastr)
-	---@type MessageData
-	local data = MessageData:CreateFromString(datastr)
-	if data ~= nil then
-		--print(Common.Dump(data.Params))
-		local ui = Ext.GetBuiltinUI("Public/Game/GUI/playerInfo.swf")
-		if ui ~= nil then
-			
-		end
-	end
-end
-
-Ext.RegisterNetListener("LLWEAPONEX_UpdateStatusMC", LLWEAPONEX_UpdateStatusMC)
-
-local function DisplayOverheadDamage(call,datastr)
-	---@type MessageData
-	local data = MessageData:CreateFromString(datastr)
+Ext.RegisterNetListener("LLWEAPONEX_DisplayOverheadDamage", function(cmd, payload)
+	local data = Common.JsonParse(payload)
 	if data ~= nil then
 		--print(Common.Dump(data.Params))
 		---@type UIObject
@@ -158,19 +85,10 @@ local function DisplayOverheadDamage(call,datastr)
 			ui:Invoke("addOverheadDamage", handle, data.Params.Text)
 		end
 	end
-end
-
-Ext.RegisterNetListener("LLWEAPONEX_DisplayOverheadDamage", DisplayOverheadDamage)
-
-Ext.RegisterListener("GameStateChanged", function(from, to)
-	if from == "Running" and to == "UnloadLevel" then
-		MasteryMenu.SetToggleButtonVisibility(false, true)
-	elseif from == "Sync" and to == "Running" then
-		--MasteryMenu.SetToggleButtonVisibility(true, true)
-	end
 end)
 
 Ext.RegisterListener("SessionLoaded", function()
+	tooltipHandler.Init()
 	local rollingText = Ext.GetTranslatedString("he38e2e7bg72dbg4477g86f9ga1fedc4f6750", "Dice Rolls")
 	CombatLog.AddFilter("Rolls", rollingText, Vars.DebugMode or nil, 3)
 	if Mods.CharacterExpansionLib then
