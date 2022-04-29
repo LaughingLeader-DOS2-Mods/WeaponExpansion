d7d44d60f64c9988a80e19a26572c20324593f4b
LaughingLeader <laughingleader@gmail.com>
2022-04-28 22:01:09 -0500

    Clear damage if it's greater than or equal to 1 for Pacifist's Wrath

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/PacifistsWrath.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/PacifistsWrath.lua
@@ -1,7 +1,7 @@
 if not Vars.IsClient then
 	--1 Damage Max
 	AttackManager.OnWeaponTagHit.Register("LLWEAPONEX_PacifistsWrath_Equipped", function(tag, attacker, target, data, targetIsObject, skill)
-		if data.Damage > 1 then
+		if data.Damage >= 1 then
 			data:ClearAllDamage()
 			--No killing if at 1 hp
 			if GameHelpers.Ext.ObjectIsCharacter(target) and target.Stats.CurrentVitality == 1 then
