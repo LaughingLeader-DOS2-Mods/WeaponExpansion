4feb8b89fd31d0e4f073d71706f92eb5fdf8a459
LaughingLeader <laughingleader@gmail.com>
2022-06-09 09:42:49 -0500

    Update Balrin's Throwing Axe logic

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/BalrinThrowingAxe.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/BalrinThrowingAxe.lua
@@ -5,7 +5,7 @@ if not Vars.IsClient then
 			SetOnStage(axeData.Item, 1)
 			NRD_CharacterEquipItem(char, axeData.Item, axeData.Slot, 0, 0, 1, 1)
 			if refreshSkill == true then
-				GameHelpers.Skill.SetCooldown(char, axeData.Skill, 0.0, true)
+				GameHelpers.Skill.SetCooldown(char, axeData.Skill, 0.0)
 			end
 			if deleteData ~= false then
 				PersistentVars.SkillData.ThrowBalrinAxe[char] = nil
@@ -26,14 +26,16 @@ if not Vars.IsClient then
 		end
 	end)
 
-	Timer.RegisterListener("Timers_LLWEAPONEX_CheckForAxeMiss", function(_, char)
-		EquipBalrinAxe(char)
-		CharacterStatusText(char, "LLWEAPONEX_StatusText_BalrinAxeTimedOut")
+	Timer.Subscribe("LLWEAPONEX_CheckForAxeMiss", function (e)
+		if e.Data.UUID then
+			EquipBalrinAxe(e.Data.UUID)
+			CharacterStatusText(e.Data.UUID, "LLWEAPONEX_StatusText_BalrinAxeTimedOut")
+		end
 	end)
 
 	---@param skill string
 	---@param char string
-	---@param state SkillState
+	---@param state SKILL_STATE
 	---@param data SkillEventData|HitData
 	local function OnBalrinAxeThrown(skill, char, state, data)
 		if state == SKILL_STATE.USED then
@@ -51,7 +53,7 @@ if not Vars.IsClient then
 			CharacterUnequipItem(char, axeData.Item)
 			SetOnStage(axeData.Item, 0)
 			Osi.ProcObjectTimer(char, "LLWEAPONEX_Timers_Throwing_BalrinAxeThrowMissed", 1200)
-			Timer.Start("Timers_LLWEAPONEX_CheckForAxeMiss", 1200, char)
+			Timer.Start("LLWEAPONEX_CheckForAxeMiss", 1200, char)
 		elseif state == SKILL_STATE.HIT then
 			GainThrowingMasteryXP(char, data.Target)
 			if not data.Success or ObjectIsItem(data.Target) == 1 then
@@ -64,9 +66,9 @@ if not Vars.IsClient then
 					DeathManager.ListenForDeath("ThrowBalrinAxe", data.Target, char, 1000)
 				end
 			end
-			Timer.Cancel("Timers_LLWEAPONEX_CheckForAxeMiss", char)
+			Timer.Cancel("LLWEAPONEX_CheckForAxeMiss", char)
 		elseif state == SKILL_STATE.PROJECTILEHIT then
-			Timer.Cancel("Timers_LLWEAPONEX_CheckForAxeMiss", char)
+			Timer.Cancel("LLWEAPONEX_CheckForAxeMiss", char)
 			if StringHelpers.IsNullOrEmpty(data.Target) then
 				EquipBalrinAxe(char)
 				CharacterStatusText(char, "LLWEAPONEX_StatusText_BalrinAxeTimedOut")
@@ -115,7 +117,7 @@ if not Vars.IsClient then
 		return false
 	end
 
-	RegisterStatusListener("Applied", "LLWEAPONEX_BALRINAXE_RECOVER_START", function(balrinUser, status, target)
+	StatusManager.Register.Applied("LLWEAPONEX_BALRINAXE_RECOVER_START", function(balrinUser, status, target)
 		if RecoverBalrinAxe(balrinUser) then
 			RemoveStatus(target, "LLWEAPONEX_WEAPON_THROW_UNIQUE_AXE1H_A")
 			ApplyStatus(target, "LLWEAPONEX_BALRINAXE_DEBUFF", 6.0, 1, balrinUser) -- No Aura
