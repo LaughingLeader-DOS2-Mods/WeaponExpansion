5f6d328b41e4ad380a14dd0735eed273ec71441a
LaughingLeader <laughingleader@gmail.com>
2022-06-24 07:47:01 -0500

    Rework firearm tactics bonus to use once subscriptions for removing the status

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Mastery/Bonuses/Firearm.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Mastery/Bonuses/Firearm.lua
@@ -5,10 +5,18 @@ MasteryBonusManager.AddRankBonuses(MasteryID.Firearm, 1, {
 	rb:Create("FIREARM_TACTICAL_RETREAT", {
 		Skills = {"Jump_TacticalRetreat", "Jump_EnemyTacticalRetreat"},
 		Tooltip = ts:CreateFromKey("LLWEAPONEX_MB_Firearm_TacticalRetreat", "<font color='#00FF99'>After jumping, your next action's AP cost is reduced by [Stats:Stats_LLWEAPONEX_MasteryBonus_Firearm_Tactics:APCostBoost].</font>"),
-	}):RegisterSkillListener(function(bonuses, skill, char, state, data)
-		if state == SKILL_STATE.CAST then
-			GameHelpers.Status.Apply(char, "LLWEAPONEX_MASTERYBONUS_FIREARM_TACTICS", -1.0, 0, char)
-		end
+	}).Register.SkillCast(function(self, e, bonuses)
+		local GUID = e.Character.MyGuid
+		GameHelpers.Status.Apply(e.Character, "LLWEAPONEX_MASTERYBONUS_FIREARM_TACTICS", -2.0, true, e.Character)
+
+		Events.OnSkillState:Subscribe(function (se)
+			GameHelpers.Status.Remove(se.Character, "LLWEAPONEX_MASTERYBONUS_FIREARM_TACTICS")
+		end, {Once=true, MatchArgs=function(_e) return _e.State == SKILL_STATE.CAST and _e.Character.MyGuid == GUID end})
+
+		Events.OnBasicAttackStart:Subscribe(function (ae)
+			GameHelpers.Status.Remove(ae.Attacker, "LLWEAPONEX_MASTERYBONUS_FIREARM_TACTICS")
+		end, {Once=true, MatchArgs=function(_e) return _e.Attacker.MyGuid == GUID end})
+
 	end),
 })
 
@@ -23,21 +31,3 @@ MasteryBonusManager.AddRankBonuses(MasteryID.Firearm, 3, {
 MasteryBonusManager.AddRankBonuses(MasteryID.Firearm, 4, {
 	
 })
-
-if not Vars.IsClient then
-	SkillManager.RegisterAnySkillListener(function(char, state, skill, skillType, element)
-		if state == SKILL_STATE.CAST and HasActiveStatus(char, "LLWEAPONEX_MASTERYBONUS_FIREARM_TACTICS") == 1 then
-			GameHelpers.Status.Remove(char, "LLWEAPONEX_MASTERYBONUS_FIREARM_TACTICS")
-		end
-	end)
-	
-	AttackManager.OnStart.Register(function(attacker, target)
-		if HasActiveStatus(attacker.MyGuid, "LLWEAPONEX_MASTERYBONUS_FIREARM_TACTICS") == 1 then
-			GameHelpers.Status.Remove(attacker.MyGuid, "LLWEAPONEX_MASTERYBONUS_FIREARM_TACTICS")
-		end
-	end)
-	
-	AttackManager.OnWeaponTagHit.Register("LLWEAPONEX_Blunderbuss_Equipped", function(tag, attacker, target, data, targetIsObject, skill)
-		GameHelpers.Skill.Explode(target, "Projectile_LLWEAPONEX_Blunderbuss_Shot_Explode", attacker)
-	end, false)
-end
