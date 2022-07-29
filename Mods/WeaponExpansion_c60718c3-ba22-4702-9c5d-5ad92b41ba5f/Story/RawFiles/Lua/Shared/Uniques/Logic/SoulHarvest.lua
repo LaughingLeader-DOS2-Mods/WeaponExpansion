1d1962281fd5783d01a1b9fa35b50d7ace7eb2d1
LaughingLeader <laughingleader@gmail.com>
2022-07-29 16:00:42 -0500

    Removed deprecated Soul Harvest statuses

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/SoulHarvest.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/SoulHarvest.lua
@@ -1,7 +1,7 @@
 if not Vars.IsClient then
-	---Tiered statuses that apply when enemies with the LLWEAPONEX_SOULHARVEST_REAP status die.
+	---Tiered statuses that apply when enemies hit by Cone_LLWEAPONEX_SoulHarvest_Reap die.
 	---@type string[]
-	Skills.Data.SoulHarvestDamageTiers  = {
+	Skills.Data.SoulHarvestDamageTiers = {
 		"LLWEAPONEX_SOULHARVEST_DAMAGE_BONUS1",
 		"LLWEAPONEX_SOULHARVEST_DAMAGE_BONUS2",
 		"LLWEAPONEX_SOULHARVEST_DAMAGE_BONUS3",
@@ -9,21 +9,26 @@ if not Vars.IsClient then
 		"LLWEAPONEX_SOULHARVEST_DAMAGE_BONUS5",
 	}
 
+	SkillManager.Register.Hit("Cone_LLWEAPONEX_SoulHarvest_Reap", function (e)
+		if e.Data.Success then
+			EffectManager.PlayEffect("RS3_FX_Skills_Soul_Cast_Target_Cast_LastRites_Body_01", e.Data.Target, {Bone="Dummy_BodyFX"})
+			DeathManager.ListenForDeath("SoulHarvestReaping", e.Data.Target, e.Character.MyGuid, 1000)
+		end
+	end)
+
 	DeathManager.RegisterListener("SoulHarvestReaping", function(target, source, targetDied)
 		if targetDied then
 			PlaySound(target, "LeaderLib_Madness_09")
-			local tier,lastTier = GameHelpers.Status.ApplyTieredStatus(source, Skills.Data.SoulHarvestDamageTiers, -1.0)
+			local tier,lastTier = GameHelpers.Status.ApplyTieredStatus(source, Skills.Data.SoulHarvestDamageTiers, -1.0, nil, source, true)
 			if tier ~= lastTier then
-				PlayEffect(source, "LLWEAPONEX_FX_Status_SoulHarvest_Impact_01", "Dummy_OverheadFX")
+				EffectManager.PlayEffect("LLWEAPONEX_FX_Status_SoulHarvest_Impact_01", source, {Bone="Dummy_OverheadFX"})
 			end
 		end
 	end)
 
-	StatusManager.Register.BeforeAttempt("LLWEAPONEX_SOULHARVEST_REAP", function(target, status, source)
-		DeathManager.ListenForDeath("SoulHarvestReaping", target, source, 1000)
+	Events.CharacterDied:Subscribe(function(e)
+		if GameHelpers.Status.IsActive(e.Character, Skills.Data.SoulHarvestDamageTiers) then
+			StatusManager.RemovePermanentStatus(e.Character, Skills.Data.SoulHarvestDamageTiers)
+		end
 	end)
-
-	-- EquipmentManager:RegisterEquipmentChangedListener(function(e)
-		
-	-- end, {Tag = "LLWEAPONEX_UniqueHarvestScythe"})
 end
