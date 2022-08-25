dc1bf5b9dbd46075dafa28170c9ac398a48c03cb
LaughingLeader <laughingleader@gmail.com>
2022-08-25 17:12:28 -0500

    Update RegisterEquippedListener usage

--- Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/Muramasa.lua
+++ Mods/WeaponExpansion_c60718c3-ba22-4702-9c5d-5ad92b41ba5f/Story/RawFiles/Lua/Shared/Uniques/Logic/Muramasa.lua
@@ -1,14 +1,14 @@
-Uniques.Muramasa:RegisterEquippedListener(function(unique, character, item, equipped)
-	if equipped then
-		if character.Stats.CurrentVitality <= (character.Stats.MaxVitality/2) then
-			GameHelpers.Status.Apply(character, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX", -1.0, false, character)
-		end
-	else
-		GameHelpers.Status.Remove(character.MyGuid, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX")
-	end
-end)
-
 if not Vars.IsClient then
+	Uniques.Muramasa:RegisterEquippedListener(function(e)
+		if e.Equipped then
+			if e.Character.Stats.CurrentVitality <= (e.Character.Stats.MaxVitality/2) then
+				GameHelpers.Status.Apply(e.Character, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX", -1.0, false, e.Character)
+			end
+		else
+			GameHelpers.Status.Remove(e.CharacterGUID, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX")
+		end
+	end)
+	
 	RegisterProtectedOsirisListener("CharacterVitalityChanged", 2, "after", function(char, percentage)
 		if GameHelpers.CharacterOrEquipmentHasTag(char, "LLWEAPONEX_UniqueMuramasaKatana") then
 			local hasWeaponEffect = HasActiveStatus(char, "LLWEAPONEX_MURAMASA_CURSE_WEAPONFX") == 1
